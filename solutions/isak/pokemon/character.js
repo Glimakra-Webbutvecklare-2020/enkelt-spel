4ef53cf9d4a15915affbd1814934ec9f31dde01c
DustiasTheGuy <isakwebdev@gmail.com>
2020-11-20 11:25:53 +0100

    don't merge

--- solutions/isak/pokemon/character.js
+++ solutions/isak/pokemon/character.js
@@ -1,25 +1,20 @@
 class Character {
     
     constructor(config) {
-        this.health = 100;
+        this.id = config.ID;
+        this.characterName = config.characterName;
         this.attackPower = config.atk;
         this.defencePower = config.def;
-        this.evadeActive = false;
-        this.id = config.ID;
         this.imageURL = config.imageURL;
-        this.renderTools = new RenderTools();
+        this.health = 100;
+        this.evadeActive = false;
     };
 
     attack(character) {
-        
-        if(!this.evadeActive) {
-            let damage = (Math.random() * 1) * (this.attackPower - character.defencePower);
-            character.health -= Math.round(damage);
-            this.evade();
-            return;
-        } else {
-            this.evadeActive = false;
-            return;
+        character.health -= ( this.attackPower - character.defencePower );
+        return { 
+            attackerHealth: this.health,
+            defenderHealth: character.health
         };
     };
 
@@ -28,37 +23,14 @@ class Character {
         this.evadeActive = probability[Math.floor(Math.random() * probability.length)];
     };
 
-    render() {
-
-        try {
-            document.getElementById(`character-${this.id}`).remove();
-        } catch(err) {
-            console.log("Initial render causes error");
-        };
-
-        let container = document.getElementsByClassName("container")[0];
-        let div = this.renderTools.getDiv(this.id);
-        let list = this.renderTools.createList();
-
-        let properties = [
+    getProps() {
+        return [
+            {key: "Character", value: this.characterName },
             {key: "Health", value: this.health}, 
             {key: "Attack Power", value: this.attackPower}, 
-            {key: "Defence", value: this.defencePower} 
-            //{key: "Player", value: this.id}
+            {key: "Evade", value: this.evadeActive },
+            {key: "Defence", value: this.defencePower}
         ]
-
-        properties.forEach(element => {
-            list.appendChild(this.renderTools.createListItem(`${element.key}: ${element.value}`));
-        });
-
-        let healthStatus = document.createElement("div");
-        healthStatus.className = "health";
-        healthStatus.style.opacity = `${calcOpacity(this.health)}%`;
-        div.appendChild(healthStatus);
-        div.appendChild(this.renderTools.createImg(this.imageURL, this.health))
-        div.appendChild(list);
-
-        container.appendChild(div);
     };
 };
 
--- solutions/isak/pokemon/main.js
+++ solutions/isak/pokemon/main.js
@@ -1,108 +1,58 @@
-let game; // global game object
+var app = new Vue({
+    el: '#app',
+    data: {
+      characterData: {
+        characterName: "",
+        imageURL: "",
+        defence: 1,
+        attack: 10
+      }, 
+      game: {
+          players: []
+      },
+      gameRunning: false
+    },
+    methods: {
+      attack() {
+         this.game.players[0].attack(this.game.players[1]);
+         this.game.players[1].attack(this.game.players[0]);
+      },
+      startGame() {
+          this.gameRunning = !this.gameRunning ? true : false;
+      },
+      createCharacter() {
 
-class RenderTools {
-    createListItem(text) {
-        let listItem = document.createElement("li");
-        listItem.textContent = text;
+        let characterAttack = parseInt(this.characterData.attack);
+        let characterDefence = parseInt(this.characterData.defence);
 
-        return listItem;
-    };
+        if(characterAttack > 10) {
+            characterDefence = 1;
+            characterAttack = 10;
+        } else if(characterDefence > 10) {
+            characterDefence = 10;
+            characterAttack = 1;
+        };
 
-    createList() {
-        return document.createElement("ul");
-    };
+        
+        this.game.players = [ // Create two new player objects and append to the game players array
+            new Character({
+                ID: 0,
+                imageURL: this.characterData.imageURL,
+                atk: characterAttack,
+                def: characterDefence,
+                characterName: this.characterData.characterName
+            }),
+            new Character({
+                ID: 1,
+                imageURL: "./cat.jpeg",
+                atk: 5,
+                def: 6,
+                characterName: "Cat"
+            })
+        ]; 
 
-    createImg(url, opacity) {
-        let tag = document.createElement("img");
-        tag.src = url;
-        return tag;
-    };
-
-    getDiv(id) {
-        let div = document.createElement("div"); // create new div element
-        div.id = `character-${id}`; // asign id
-        div.className = "character"; // asign class 
-
-        return div; 
-    };
-};
-
-
-class Game {
-
-    constructor() {
-        this.players = [];
-    };
-
-
-    render() {
-        this.players.forEach(player => {
-            player.render();
-        });
-    };
-
-    start() {
-        this.render();
-    };
-};
-
-function attackHandler() {
-    game.players[0].attack(game.players[1]);
-    game.players[1].attack(game.players[0]);
-    game.players[0].render();
-    game.players[1].render();
-
-    console.log(game);
-
-    if(game.players[0].health <= 0) {
-        alert("Cat wins!")
-        newGame();
-
-    } else if(game.players[1].health <= 0) {
-        alert("You have defeated the cat!")
-        newGame();
-
-    } else if(game.players[0].health <= 0 && game.players[1].health <= 0) {
-        alert("NO winner!")
-        newGame();
-
-    };
-
-
-    return;
-};
-
-function newGame() {
-    parseValues();
-    
-    game = new Game();
-    let characterStats = parseValues();
-    let totalPoints = characterStats.atk + characterStats.def;
-
-    game.players = [
-        new Character({
-            ID: 0, 
-            imageURL: "https://images.pexels.com/photos/1716861/pexels-photo-1716861.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-            atk: characterStats.atk,
-            def: characterStats.def
-        }), 
-        new Character({
-            ID: 1, 
-            imageURL: "https://images.pexels.com/photos/4438556/pexels-photo-4438556.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-            atk: totalPoints / 2,
-            def: totalPoints / 2
-        })
-    ] 
-
-    game.start();
-    document.getElementById("atkButton").style.display = "block";
-    document.getElementsByClassName("character-creation")[0].style.display = "none";
-
-};
-
-function parseValues() {
-    let atk = document.getElementById("atk").value;
-    let def = document.getElementById("def").value;
-
-    return { atk: parseInt(atk) + 10, def: parseInt(def) };
-};
+        console.log(this.game.players[0])
+        this.startGame();
+      }
+    }
+});
