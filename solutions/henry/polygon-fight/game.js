4bf4b5aabfc78e8e233da7507e2472d2f7b2a014
frozenbanana <henrybergstrom@protonmail.com>
2020-12-02 21:38:03 +0100

    user can chose character

--- solutions/henry/polygon-fight/game.js
+++ solutions/henry/polygon-fight/game.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+/* Selection */
+// Inital
+const polygons = document.querySelectorAll(".polygon");
+let selectedPolygon = undefined;
+
+// Functions
+const setSelectedPolygon = (event) => {
+    // clear old selected just in case
+    polygons.forEach((polygon) => {
+        if (polygon.classList.contains("selected")) {
+            polygon.classList.remove("selected");
+        }
+    });
+    selectedPolygon = event.target;
+    selectedPolygon.classList.add("selected");
+};
+
+// Setup
+polygons.forEach((polygon) => {
+    polygon.addEventListener("click", setSelectedPolygon);
+});
