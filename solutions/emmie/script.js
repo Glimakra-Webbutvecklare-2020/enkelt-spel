d78a50a9f8abf3e50ee036cf71adc2c802a9111d
7nnesson <emmie.sjunnesson@gmail.com>
2020-12-06 16:33:44 +0100

    changes

--- solutions/emmie/script.js
+++ solutions/emmie/script.js
@@ -1,34 +1,84 @@
-function ask(question) {
-    let userAnswer = prompt("This is question").toLowerCase();
+function ask(mood, randomNumber) {
+    // let userAnswer = prompt(question).toLowerCase();
     let response = undefined;
 
-    switch (userAnswer) {
-        case "option1":
-            console.log("Since you that was your choice, then this happens!");
-            response = "response1";
+    switch (mood) {
+            // 1
+        case "happy":
+            if (randomNumber === 1){
+                response = "It's a tie, both of us picked happy";
+            } else if (randomNumber === 2){
+                response = "Computer wins, sleepy beats happy"
+            } else if (randomNumber === 3){
+                response = "You win, happy beats sad"
+            }
             break;
-        case "option2":
-            console.log(
-                "Since you that was your choice, then another thing happens!"
-            );
-            response = "response2";
+            // 2
+        case "sleepy":
+            if (randomNumber === 1){
+                response = "You win, sleepy beats happy";
+            } else if (randomNumber === 2){
+                response = "It's a tie, both of us picked sleepy";
+            } else if (randomNumber === 3){
+                response = "Computer wins, sad beats sleepy"
+            }
             break;
-        default:
-            console.log("Not a valid response");
+            // 3
+        case "sad":
+            if (randomNumber === 1){
+                response = "Computer wins, happy beats sad";
+            } else if (randomNumber === 2){
+                response = "You win, sad beats sleepy";
+            } else if (randomNumber === 3){
+                response = "It's a tie, both of us picked sad"
+            }
+            break;
     }
-
     return response;
 }
+const moods = document.querySelector("#moods");
+const winner = document.querySelector("#winner");
+const computer = document.querySelector("#computer");
+moods.addEventListener("click", function(event){
+    console.log(event.target);
+    const mood = event.target.getAttribute("data-id");
+    console.log(mood);
 
-const gameName = "A game";
-let play = prompt(`Do you want to play ${gameName}?`).toLowerCase();
-
-let isPlaying = play == "y" || play == "yes";
-
-while (isPlaying) {
-    response = ask("What is your next move?");
-
-    console.log(response);
+    // computer random
+    let randomNumber = randomChoice();
+    let answer = ask(mood, randomNumber);
+    
+    // timeout
+setTimeout(function() {
+    console.log(answer);
+    winner.innerHTML = answer;
+    computer.innerHTML = getMood(randomNumber);
+}, 1000)
+    
+})
+function getMood (number) {
+    if (number === 1) {
+        return "happy";
+    } else if (number === 2) {
+        return "sleepy";
+    } else if (number === 3) {
+        return "sad";
+    }
+}
+function randomChoice() {
+    let randomNumber = Math.random();
+    return Math.ceil(randomNumber * 3);
 }
 
-alert("Good bye!");
+const gameName = "Elephant moodgame";
+
+let isPlaying = true
+let counter = 0;
+while (isPlaying) {
+    if (counter >= 3) {
+        isPlaying = false;
+    }
+    // response = ask("Yey! What mood do you want to play? Happy, sleepy or sad");
+
+    counter++;
+}
